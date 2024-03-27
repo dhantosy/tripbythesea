b7708dc119f093239611f3d30dc9fc328ae91328
dhanto santika <dhantosy@gmail.com>
2024-03-27 15:55:43 +0800

    feat: initial commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFoundPage() {
+  return (
+    <div>
+      <div>
+        <div>404</div>
+        <div>Page Not Found</div>
+        <div>Back to Home</div>
+      </div>
+    </div>
+  )
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Home from './_home/page';
+
+export default function LandingPage() {
+  return (
+    <main>
+      <Home />
+    </main>
+  )
+}

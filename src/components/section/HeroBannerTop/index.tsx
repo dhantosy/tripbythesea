deacaafc344859461fb0443da7b8dec9b2000249
dhanto santika <dhantosy@gmail.com>
2024-03-28 11:47:03 +0800

    feat: updated backbone structures

---
+++ src/components/section/HeroBannerTop/index.tsx
@@ -0,0 +1,8 @@
+export default function HeroBannerTop() {
+
+  return (
+    <section>
+      
+    </section>
+  )
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: '320px',
+      'xs-2': '380px',
+      'xs-3': '425px',
+      sm: '480px',
+      'sm-2': '540px',
+      'sm-3': '640px',
+      md: '768px',
+      'md-2': '860px',
+      'md-3': '980px',
+      lg: '1024px',
+      'lg-2': '1180px',
+      'lg-3': '1280px',
+      xl: '1440px',
+      'xl-2': '1600px',
+      'xl-3': '1800px',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
